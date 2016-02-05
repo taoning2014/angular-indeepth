6e05a9a3236d6949e9996bbe05dad91c871f2bd9
Tao Ning <ningtaous@gmail.com>
2016-02-04 23:53:49 -0500

    play with filter

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,28 +1,52 @@
-app.controller('MainCtrl', [function() {
-    var self = this;
-    self.notes = {
-        shyam: {
-            id: 1,
-            label: 'First Note',
-            assignee: 'Tao',
-            done: false
-        },
-        Misko: {
-            id: 3,
-            label: 'Finished Third Note',
-            assignee: 'Ning',
-            done: true
-        },
-        brad: {
-            id: 2,
-            label: 'Second Note',
-            done: false
-        }
-    };
-    self.getNodeClass = function(status) {
-        return {
-            done: status,
-            pending: !status
-        }
+app
+.controller('MainCtrl', [function() {
+  this.notes = [{
+    label: 'FC Todo',
+    type: 'chore',
+    done: false
+  }, {
+    label: 'FT Todo',
+    type: 'task',
+    done: false
+  }, {
+    label: 'FF Todo',
+    type: 'fun',
+    done: true
+  }, {
+    label: 'SC Todo',
+    type: 'chore',
+    done: false
+  }, {
+    label: 'ST Todo',
+    type: 'task',
+    done: true
+  }, {
+    label: 'SF Todo',
+    type: 'fun',
+    done: true
+  }, {
+    label: 'TC Todo',
+    type: 'chore',
+    done: false
+  }, {
+    label: 'TT Todo',
+    type: 'task',
+    done: false
+  }, {
+    label: 'TF Todo',
+    type: 'fun',
+    done: false
+  }];
+  this.sortOrder = ['+type', '-label'];
+  this.filterOptions = {
+    "string": '',
+    "object": {
+      done: false,
+      label: 'C'
+    },
+    "function": function(note) {
+      return note.type === 'task' && note.done === false;
     }
+  };
+  this.currentFilter = 'string';
 }]);
